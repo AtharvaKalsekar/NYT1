d9170d260bf7e850a0aeaceab537bfd2504f7f79
AtharvaKalsekar <kalsekar.atharva7@gmail.com>
2022-10-24 18:21:00 +0530

    adds tab icons

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { store } from "@store";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,8 +13,25 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="TopStories" component={NewsFeed} />
+        <Tab.Navigator
+          sceneContainerStyle={styles.container}
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#006494",
+            },
+            headerTintColor: "white",
+          }}
+        >
+          <Tab.Screen
+            options={{
+              title: "Top stories",
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="newspaper-outline" size={size} color={color} />
+              ),
+            }}
+            name="TopStories"
+            component={NewsFeed}
+          />
         </Tab.Navigator>
       </NavigationContainer>
       <StatusBar style="auto" />
@@ -25,7 +43,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
